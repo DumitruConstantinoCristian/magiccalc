551f9300a4ca4e0204815797e9bd4eb6debec44b
DumitruConstantinoCristian <144817378+DumitruConstantinoCristian@users.noreply.github.com>
2023-09-26 16:01:36 +0100

    Add files via upload

---
+++ PythonApplication6/PythonApplication6.py
@@ -0,0 +1,21 @@
+print("Hi im Mystical Meredith  ")
+print("What is your name?")
+input1=input("Enter your name")
+print("Please pick a number between 1 and 10")
+input2=input("Pick a number ")
+original_number= float(input2)
+print("Your number multiplied by 2 is")
+result1= int(original_number)* 2
+print(result1)
+print("This is your result multiplied by 5")
+result2= int(result1)*5
+print(result2)
+print("This is your last result divided by the original number that u chose")
+result3=float(result2) / original_number
+print(result3)
+print("This should be 3")
+result4=result3-7
+print(result4)
+
+
+
